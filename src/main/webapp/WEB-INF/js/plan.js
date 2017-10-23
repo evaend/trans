152a01c5f7c2799fee3b7f3d389de48b6a1d7640
sarah0730 <943349411@qq.com>
2017-10-23 15:51:00 +0800

    添加了默认日期(当前日期)
    update

--- src/main/webapp/WEB-INF/js/plan.js
+++ src/main/webapp/WEB-INF/js/plan.js
@@ -2234,9 +2234,7 @@ toolbar: [{
 title: '发运计划信息表',
 url: '../transController/searchDispatchInfo',
 height: __WEBPACK_IMPORTED_MODULE_7_jQuery___default()(window).height() - 150,
-columns: [[{ field: 'consignDetailId', checkbox: true }, { field: 'createDate', title: '制单日期', width: 150 }, { field: 'despatchDate', title: '发运日期', width: 120 }, { field: 'consignNo', title: '运单号', width: 200 }, { field: 'chassisNo', title: '底盘号', width: 200 }, { field: 'amount', title: '数量', width: 70, align: 'right' }, { field: 'locationFrom', title: '起运地', width: 100 }, { field: 'locationTo', title: '目的地', width: 100 }, { field: 'carBrand', title: '品牌', width: 100 }, { field: 'carModel', title: '车型', width: 100 }, { field: 'transportTool', title: '轿运车', width: 150 },
-//{field:'name9',title:'车次',width:100}, 
-{ field: 'consignOrgName', title: '托运单位', width: 260 }, { field: 'receiveOrgName', title: '收车单位', width: 260 }, { field: 'carrierName', title: '承运商', width: 260 }, { field: 'remark', title: '备注', width: 260 }]]
+columns: [[{ field: 'consignDetailId', checkbox: true }, { field: 'createDate', title: '制单日期', width: 150 }, { field: 'despatchDate', title: '发运日期', width: 120 }, { field: 'consignNo', title: '运单号', width: 200 }, { field: 'chassisNo', title: '底盘号', width: 200 }, { field: 'amount', title: '数量', width: 70, align: 'right' }, { field: 'locationFrom', title: '起运地', width: 100 }, { field: 'locationTo', title: '目的地', width: 100 }, { field: 'carBrand', title: '品牌', width: 100 }, { field: 'carModel', title: '车型', width: 100 }, { field: 'transportTool', title: '轿运车', width: 150 }, { field: 'consignOrgName', title: '托运单位', width: 260 }, { field: 'receiveOrgName', title: '收车单位', width: 260 }, { field: 'carrierName', title: '承运商', width: 260 }, { field: 'remark', title: '备注', width: 260 }]]
 });
 
 //查询按钮
@@ -2245,7 +2243,7 @@ var postData = __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_4__utils_common__
 //刷新表格
 __WEBPACK_IMPORTED_MODULE_0__component_table__["a" /* table */].load($grid, postData);
 });
-
+__WEBPACK_IMPORTED_MODULE_0__component_table__["a" /* table */].load($grid, { createDateStart: show(), createDateEnd: show() });
 __WEBPACK_IMPORTED_MODULE_1__component_window__["a" /* modalWindow */].init($dialog, { title: '车辆信息' }, function (text) {
 var postData = __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_4__utils_common__["a" /* getFormData */])($form);
 //===== 校验 =======
@@ -2408,6 +2406,14 @@ if (files.length === 0) {
   });
 }
 });
+
+function show() {
+var mydate = new Date();
+var str = "" + mydate.getFullYear() + "-";
+str += mydate.getMonth() + 1 + "-";
+str += mydate.getDate();
+return str;
+}
 
 /***/ })
 /******/ ]);
